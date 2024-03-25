e74435d1e96559d3333f5a06f6e3832e8beeb2e8
adriijan <adriana.domorakova@gnj.cz>
2024-03-25 22:48:04 +0100

    First commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* INDEX - changing photos*/
+
 const images = document.querySelectorAll('.image');
 let currentIndex = 0;
 
@@ -21,23 +23,68 @@ function showNextImage() {
 
 setInterval(showNextImage, 2000); // Start the loop
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 	resizeImageContainer();
-  });
-  
-  window.addEventListener('resize', function() {
+});
+
+window.addEventListener('resize', function () {
 	resizeImageContainer();
-  });
-  
-  function resizeImageContainer() {
+});
+
+function resizeImageContainer() {
 	var imageContainer = document.getElementById('imageContainer');
 	var images = imageContainer.querySelectorAll('.image');
-  
+
 	var maxHeight = 0;
-	images.forEach(function(image) {
-	  maxHeight = Math.max(maxHeight, image.offsetHeight);
+	images.forEach(function (image) {
+		maxHeight = Math.max(maxHeight, image.offsetHeight);
 	});
-  
+
 	imageContainer.style.height = maxHeight + 'px';
-  }
-  
+}
+
+/* HAMBURGER MENU*/
+
+document.addEventListener('DOMContentLoaded', function () {
+	const iconBars = document.querySelector("#menu-icon__bars");
+	const iconCross = document.querySelector("#menu-icon__cross");
+	const menu = document.querySelector(".menu");
+	const body = document.querySelector("body");
+	const header = document.querySelector("header");
+	const imageContainer = document.getElementById("imageContainer");
+
+
+
+	function showSidePanel() {
+		menu.classList.remove('menu');
+		menu.classList.add('sidePanel');
+		iconBars.classList.remove('menu-icon__bars');
+		iconCross.classList.add('menu-icon__cross');
+		body.classList.add('show-overlay');
+		header.style.backgroundColor = "unset";
+		imageContainer.style.zIndex = '0';
+	}
+
+
+	function hideSidePanel() {
+		menu.classList.remove('sidePanel');
+		menu.classList.add('menu');
+		iconBars.classList.add('menu-icon__bars');
+		iconCross.classList.remove('menu-icon__cross');
+		body.classList.remove('show-overlay');
+		header.style.backgroundColor = "white";
+		imageContainer.style.zIndex = '1';
+	}
+
+
+	iconBars.addEventListener('click', function () {
+		showSidePanel();
+
+	});
+
+
+	iconCross.addEventListener('click', function () {
+		hideSidePanel();
+	});
+
+});
